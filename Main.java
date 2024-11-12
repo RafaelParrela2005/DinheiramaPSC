9adec1378b30f22aecaa8d4740bcf2561c7388cd
RafaelParrela2005 <RafaelParrela2005@hotmail.com>
2024-11-12 18:41:16 -0300

    01

--- Main.java
+++ Main.java
@@ -2,9 +2,12 @@ import javax.swing.JFrame;
 
 public class Main {
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Janela Vazia");
+        JFrame frame = new JFrame("Dinheirama");
 
-        frame.setSize(400, 300);
+        frame.setSize(800, 500);
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.setLayout(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         frame.setVisible(true);
