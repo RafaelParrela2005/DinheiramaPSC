2eb4af290cf354f5a6d2d3bdd5ee3982325f804f
RafaelParrela2005 <RafaelParrela2005@hotmail.com>
2024-11-12 22:53:25 -0300

    01

--- Main.java
+++ Main.java
@@ -1,14 +1,20 @@
-import javax.swing.JFrame;
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Dinheirama");
+
+        JButton test_button = new JButton("Teste");
+        test_button.setFocusable(false);
+        test_button.setBounds(10, 10, 100, 30);
 
         frame.setSize(800, 500);
         frame.setResizable(false);
         frame.setLocationRelativeTo(null);
         frame.setLayout(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.add(test_button);
 
         frame.setVisible(true);
     }
