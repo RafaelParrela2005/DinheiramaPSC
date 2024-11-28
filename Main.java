026b86ca75b038d0faefdc1a7c758611dbeee067
RafaelParrela2005 <RafaelParrela2005@hotmail.com>
2024-11-28 18:39:57 -0300

    commit

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-//code
+
 public class Main extends JFrame {
 
     //Numeric variables
